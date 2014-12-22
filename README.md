4f352095c1f02a49f4d70bd1386cb51ac20d6376
rcbull <bnw_bull@yahoo.com.br>
2014-12-21 23:56:36 -0200

    Add run_analysis.R
    Add run_analysis.R and update README.md

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 GettingAndCleaningData
 ======================
+# Description
 
-Getting And Cleaning Data - Coursera
+This repository contains scripts for creating a tidy dataset from the data provided at:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Files in the repository:
+
+* TidyData.txt - the tidy data
+* run_analysis.R - the program in R script with the recipe used to generate the tidy data set
+* README.md - this file
+* UCI HAR Dataset - folder with data
+
+Instructions
+* Create a directory on your local machine
+* Download the "getdata-projectfiles-UCI HAR Dataset.zip" file to that directory
+* Extract the zip file to folder "UCI HAR Dataset"
+* Set working directory in "run_analysis.R" script in that same directory created
+* Execute the "run_analysis.R" script with Ctrl+A and Ctrl+R
+* Results will be written to tidy dataset.txt
---
+++ run_analysis.R
@@ -0,0 +1,63 @@
+
+## set working directory
+setwd("G:/github/GettingAndCleaningData")
+
+# folder with data
+folder <- "UCI HAR Dataset"
+
+# columns be read
+# features data mean() and std()
+filename <- sprintf("%s/features.txt", folder)
+conn <- file(filename, "r")
+idx <- grep("mean\\(\\)|std\\(\\)", readLines(conn))
+close(conn)
+
+# function read:
+# this function to read files
+make.read <- function(directory, index = idx){
+  subject.df <- read.table(sprintf("%s/%s/subject_%s.txt",
+                                   folder,directory, directory))
+  y.df <- read.table(sprintf("%s/%s/y_%s.txt",
+                             folder, directory, directory))
+  X.df <- read.table(sprintf("%s/%s/X_%s.txt",
+                             folder, directory, directory))[, index]
+  dataset <- cbind(subject.df, y.df, X.df)
+  
+  # merge activity labels to sensor data
+  activity.df <- read.table(sprintf("%s/activity_labels.txt", folder))
+  suppressWarnings(dataset <- merge(dataset, activity.df, by.x = 2,
+                                    by.y = 1))
+  dataset[1] <- NULL
+  
+  dataset <- dataset[c(1, dim(dataset)[2], seq(2, dim(dataset)[2]-1))]
+  dataset
+}
+
+# combined the train and test data frames
+train.df <- make.read("train")
+test.df <- make.read("test")
+clean.df <- rbind(train.df, test.df)
+
+# process features to be used as column names for the tidy dataset
+hdr <- read.table(filename, row.names = NULL)[idx, 2]
+hdr <- gsub("-", ".", hdr)
+hdr <- gsub("\\(\\)", "", hdr)
+hdr <- sub("^[t]", "Time", hdr)
+hdr <- sub("^[f]", "Freq", hdr)
+hdr <- sub("(Body)+", "Body", hdr)
+colnames(clean.df) <- c("subject.id", "activity", hdr)
+
+# ordering tidy dataset rows by subject then activity
+clean.df <- clean.df[order(clean.df$subject.id, clean.df$activity), ]
+row.names(clean.df) <- NULL
+
+# uses the aggregate function to summarize
+tidy.df <- with(clean.df,
+                aggregate(clean.df[c(seq(3, dim(clean.df)[2]))],
+                          by = list(subject.id, activity),
+                          mean))
+colnames(tidy.df) [1]<- "subject"
+colnames(tidy.df)[2] <- "activity"
+
+# saves the aggregated dataset for reporting
+write.table(tidy.df, file = "tidy dataset.txt", row.names = FALSE)
