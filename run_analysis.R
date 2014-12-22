0b311d37a8e23e69856a7de52682ab67f6132387
rcbull <bnw_bull@yahoo.com.br>
2014-12-22 00:04:57 -0200

    update
    update run_analyses.R

--- run_analysis.R
+++ run_analysis.R
@@ -60,4 +60,4 @@ colnames(tidy.df) [1]<- "subject"
 colnames(tidy.df)[2] <- "activity"
 
 # saves the aggregated dataset for reporting
-write.table(tidy.df, file = "tidy dataset.txt", row.names = FALSE)
+write.table(tidy.df, file = "TidyData.txt", row.names = FALSE)
